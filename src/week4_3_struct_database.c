56ee33482471428db72005e72091b37228eef047
Ayma Rehman <ayma.rehman24@gmail.com>
2025-10-06 07:45:32 +0000

    Completed week4_3_struct_database.c

--- src/week4_3_struct_database.c
+++ src/week4_3_struct_database.c
@@ -1,7 +1,7 @@
 /*
  * week4_3_struct_database.c
- * Author: [Your Name]
- * Student ID: [Your ID]
+ * Author: Ayma Rehman
+ * Student ID: 241ADB165
  * Description:
  *   Simple in-memory "database" using an array of structs.
  *   Students will use malloc to allocate space for multiple Student records,
@@ -12,7 +12,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-// TODO: Define struct Student with fields name, id, grade
+// Define struct Student with fields name, id, grade
+struct Student {
+    char name[50];
+    int id;
+    float grade;
+};
 
 int main(void) {
     int n;
@@ -24,15 +29,43 @@ int main(void) {
         return 1;
     }
 
-    // TODO: Allocate memory for n Student structs using malloc
+    // Allocate memory for n Student structs using malloc
+    students = (struct Student *)malloc(n * sizeof(struct Student));
+    if (students == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
 
-    // TODO: Read student data in a loop
+    // Read student data in a loop
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter data for student %d:\n", i + 1);
 
-    // TODO: Display all student records in formatted output
+        printf("Name: ");
+        scanf("%s", students[i].name); // Use fgets() if you want to allow spaces in names
 
-    // Optional: Compute average grade or find top student
+        printf("ID: ");
+        scanf("%d", &students[i].id);
 
-    // TODO: Free allocated memory
+        printf("Grade: ");
+        scanf("%f", &students[i].grade);
+    }
+
+    // Display all student records in formatted output
+    printf("\n%-6s %-20s %-6s\n", "ID", "Name", "Grade");
+    printf("----------------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf("%-6d %-20s %-6.2f\n", students[i].id, students[i].name, students[i].grade);
+    }
+
+    // Optional: Compute average grade
+    float total = 0;
+    for (int i = 0; i < n; i++) {
+        total += students[i].grade;
+    }
+    printf("\nAverage grade of class: %.2f\n", total / n);
+
+    // Free allocated memory
+    free(students);
 
     return 0;
 }
